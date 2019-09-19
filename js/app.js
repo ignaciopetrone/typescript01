ede88ff83d66e5d596dfe2218f8bd2587846acc5
Ignacio Petrone <ipetrone@Santiagos-MacBook-Pro.local>
2019-09-19 19:42:06 +0200

    last

---
+++ js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+function startGame() {
+    // startgin a new game
+    var messagesElement = document.getElementById("messages");
+    messagesElement.innerText = "Welcome to Jurassic Park";
+}
+document.getElementById("startGame").addEventListener("click", startGame);
+//# sourceMappingURL=app.js.map
