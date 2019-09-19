7648e0641342c550153397e270ad3a8e36882eaa
Ignacio Petrone <ipetrone@Santiagos-MacBook-Pro.local>
2019-09-19 19:41:35 +0200

    proyect working

--- app/app.js
+++
@@ -1,6 +0,0 @@
-function startGame() {
-    // startgin a new game
-    var messagesElement = document.getElementById("messages");
-    messagesElement.innerText = "Welcome to Jurassic Park";
-}
-document.getElementById("startGame").addEventListener("click", startGame);
--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 function startGame() {
   // startgin a new game
   var messagesElement = document.getElementById("messages");
-  messagesElement.innerText = "Welcome to Jurassic Park";
+  messagesElement!.innerText = "Welcome to Jurassic Park";
 }
 
-document.getElementById("startGame").addEventListener("click", startGame);
+document.getElementById("startGame")!.addEventListener("click", startGame);
