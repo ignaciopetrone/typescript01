8e5218a89ed78c0fa29ccada767f3da0a1056fdd
Ignacio Petrone <ipetrone@Santiagos-MacBook-Pro.local>
2019-09-19 20:16:47 +0200

    compiling typescript with webpack

--- js/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function startGame() {
-    // startgin a new game
-    var messagesElement = document.getElementById("messages");
-    messagesElement.innerText = "Welcome to Jurassic Park";
-}
-document.getElementById("startGame").addEventListener("click", startGame);
-//# sourceMappingURL=app.js.map
